e9469613453a302846abd910c2922e559858ce96
scienceopen <scienceopen@users.noreply.github.com>
2014-07-18 19:06:23 -0400

    Trapped out C-ordered U (needs to be F-order)

--- picard.py
+++ picard.py
@@ -1,6 +1,7 @@
 import numpy as np
 from warnings import warn
 import matplotlib.pyplot as plt
+import pdb
 #PICARD Visual inspection of the Picard condition.
 #
 # eta = picard(U,s,b,d)
@@ -23,9 +24,14 @@ import matplotlib.pyplot as plt
 # Per Christian Hansen, IMM, April 14, 2001.
 # ported to Python by Michael Hirsch
 def picard(U,s,b,d=0):
+    if U.flags['F_CONTIGUOUS'] is False:
+        raise RuntimeError('U must be Fortran-ordered for this function to work! try U = np.copy(U,order="F")')
+
+    pdb.set_trace()
     n,ps = np.atleast_2d(s).T.shape # the transpose is because Numpy 1.8.1 has no order='F' option for atleast2d
 
     beta = np.abs( U[:,:n].T.dot(b) )
+    pdb.set_trace()
     eta = np.zeros(n,order='F')
 
     if ps==2: s = s[:,0] / s[:,1]
@@ -33,20 +39,21 @@ def picard(U,s,b,d=0):
     d21 = 2 * d + 1
     keta = np.arange(d,n-d)
 
-    if np.any(np.isclose(s,0)):
+    if np.any(s==0): #10**-14 is OK?
         warn('picard: Division by zero: singular values')
 
-    #for ik,k in enumerate(keta):
-    #    eta[ik] = ( np.prod(beta[ik-d:ik+d])**(1/d21)) /s[ik]
     for i in keta:
         es = np.s_[i-d:i+d+1]
         eta[i] = ( np.prod(beta[es])**(1/d21)) / s[i]
 #%% plot Picard plot
     ni = np.arange(n)
-    plt.figure()
-    plt.semilogy(ni, s, '.-', ni, beta, 'x', keta, eta[keta], 'o')
+    plt.figure(879832)
+    plt.semilogy(ni, s, '.-') #breaks for inf
+    plt.semilogy(ni, beta, 'x') #breaks for inf
+    plt.semilogy(keta, eta[keta], 'o')
     plt.xlabel('i')
     plt.title('Picard plot')
+    #plt.autoscale(True,tight=True)
     if ps==1:
         plt.legend( ('$\sigma_i$','$|u_i^T b|$','$|u_i^T b|/\sigma_i$'),loc='lower left' )
     else:
