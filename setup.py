fd902efd7c4575cda94ae7bd0bbf7e0694b23154
Michael Hirsch <scienceopen@users.noreply.github.com>
2015-10-09 11:58:43 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup 
 
-with open('README.rst') as f:
+with open('README.rst','r') as f:
 	long_description = f.read()
 	
 setup(name='pyAIRtools',
@@ -8,7 +8,6 @@ setup(name='pyAIRtools',
 	  description='Python port of Matlab AIRtools and ReguTools regularization toolbox',
 	  long_description=long_description,
 	  author='Michael Hirsch',
-	  author_email='hirsch617@gmail.com',
 	  url='https://github.com/scienceopen/pyAIRtools',
 	  install_requires=['numpy','scipy'],
       packages=['airtools'],
