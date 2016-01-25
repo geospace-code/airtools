a0da33c3ecb0999014cebf624c08f481e83d8757
Michael Hirsch <scienceopen@users.noreply.github.com>
2016-01-24 21:42:25 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup 
+import subprocess
 
 with open('README.rst','r') as f:
 	long_description = f.read()
@@ -12,3 +13,8 @@ setup(name='pyAIRtools',
       packages=['airtools'],
 	  )
 
+#%%
+try:
+    subprocess.call(['conda','install','--yes','--quiet','--file','requirements.txt'],shell=False) #don't use os.environ
+except Exception as e:
+    print('you will need to install packages in requirements.txt  {}'.format(e))
