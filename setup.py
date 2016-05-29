4859b918f10d3810bdbda9b2a75a5658c7429107
scienceopen <scienceopen@users.noreply.github.com>
2016-05-29 02:55:53 -0400

    new template

--- setup.py
+++ setup.py
@@ -1,20 +1,18 @@
 from setuptools import setup 
 import subprocess
 
+try:
+    subprocess.run(['conda','install','--yes','--file','requirements.txt'])
+except Exception as e:
+    pass
+
 with open('README.rst','r') as f:
 	long_description = f.read()
 	
 setup(name='pyAIRtools',
-      version='0.1',
 	  description='Python port of Matlab AIRtools and ReguTools regularization toolbox',
 	  long_description=long_description,
 	  author='Michael Hirsch',
 	  url='https://github.com/scienceopen/pyAIRtools',
-      packages=['airtools'],
 	  )
 
-#%%
-try:
-    subprocess.call(['conda','install','--yes','--quiet','--file','requirements.txt'],shell=False) #don't use os.environ
-except Exception as e:
-    print('you will need to install packages in requirements.txt  {}'.format(e))
