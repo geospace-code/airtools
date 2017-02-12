ff7a0e6ad0c7c7c498c9e93627697ee51945b534
michael <hirsch617@gmail.com>
2017-02-12 02:52:22 -0500

    prerewq

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 req = ['nose','numpy','scipy','matplotlib']
 
 setup(name='pyAIRtools',
+      packages=['airtools'],
 	  description='Python port of Matlab AIRtools and ReguTools regularization toolbox',
 	  url='https://github.com/scienceopen/pyAIRtools',
 	  author='Michael Hirsch, Ph.D.',
-      packages=['airtools'],
       install_requires=req,
       extras_require={'cvxopt':['cvxopt']},
 	  )
